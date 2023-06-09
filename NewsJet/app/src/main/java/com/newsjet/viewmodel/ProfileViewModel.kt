f0dd31f14d7f15c539ff50ce8b6fb9959a5d9237
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-06-09 19:14:13 +0530

    Create NewsJet/app/src/main/java/com/newsjet/viewmodel/ProfileViewModel.kt

---
+++ NewsJet/app/src/main/java/com/newsjet/viewmodel/ProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.newsjet.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ProfileViewModel @Inject constructor() : ViewModel() {
+    // Add any necessary logic for the Profile screen here
+}
